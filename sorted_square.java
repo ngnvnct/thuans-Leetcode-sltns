23cb751b911e7cc20789f4a86b2c538557305e40
Thuan Le <gile@ucsc.edu>
2022-02-06 19:13:18 -0800

    explanation for sorted square

--- sorted_square.java
+++ sorted_square.java
@@ -34,3 +34,14 @@ public class sorted_square {
         assertArrayEquals(expected, sortedSquare(exampleTwo));
     }
 }
+
+/*
+Explanation
+
+The key take away is the array is sorted. So the start and the end element have the potential to be the largest. We take the absolute value of the start and end element and use Two Pointers approach
+Then we compare it with each other, and insert the bigger one square at the end of our result array.
+
+Time: O(n), where n is the array
+Space: O(n), where n is the array input
+
+*/
