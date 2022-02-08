1a8723c97a5ed1fe6d132070f939b1a0a5cb4074
Thuan Le <gile@ucsc.edu>
2022-02-07 21:35:13 -0800

    add max sub array and min cost climbing star DP and array

---
+++ max_subarray.java
@@ -0,0 +1,43 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class max_subarray {
+    public int maxSubArray(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currSum = Math.max(nums[i], currSum + nums[i]);
+            maxSum = Math.max(maxSum, currSum);
+        }
+        return maxSum;
+    }
+
+    @Test
+    public void testMaxSubarray() {
+        int[] exampleOne = new int[] {-2,2,5,-11,6};
+        assertEquals(7, maxSubArray(exampleOne));
+
+        int[] exampleTwo = new int[] {-2,1,-3,4,-1,2,1,-5,4};
+        assertEquals(6, maxSubArray(exampleTwo));
+
+        int[] exampleThree = new int[] {1};
+        assertEquals(1, maxSubArray(exampleThree));
+
+        int[] exampleFour = new int[] {5,4,-1,7,8};
+        assertEquals(23, maxSubArray(exampleFour));
+    }
+}
+
+
+/*
+Using Kadane's Algorithm, we loop through the array once and see if we should make a newSum or adding it to our currentSum
+
+Time: O(n)
+Space: O(1)
+
+*/
