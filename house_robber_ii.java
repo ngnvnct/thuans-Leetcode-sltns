2616e0f2d07e3ac3192daef9c9d1c2bd95d73005
Thuan Le <gile@ucsc.edu>
2022-02-08 18:14:13 -0800

    house robber

---
+++ house_robber_ii.java
@@ -0,0 +1,62 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class house_robber_ii {
+    public int houseRobber(int[] nums) {
+        int n = nums.length;
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return nums[0];
+        }
+        if (n == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        return Math.max(helperHouseRobberI(nums, 0, n-2), helperHouseRobberI(nums, 1, n-1));
+        //return Math.max(helperHouseRobberISpace(nums, 0, n-2), helperHouseRobberI(nums, 1, n-1));
+    }
+
+    public int helperHouseRobberI(int[] nums, int start, int end) {
+        int[] dp = new int[end-start+1];
+        dp[0] = nums[start];
+        dp[1] = Math.max(nums[start], nums[start+1]);
+
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(nums[start+i] + dp[i-2], dp[i-1]);
+        }
+        return dp[dp.length-1];
+    }
+
+    public int helperHouseRobberISpace(int[] nums, int start, int end) {
+        int prevRob = 0;
+        int prevNotRob = 0;
+        int rob = 0;
+        int notRob = 0;
+        for (int i = start; i <= end; i++) {
+            rob = prevNotRob + nums[i];
+            notRob = Math.max(prevRob, prevNotRob);
+            prevNotRob = notRob;
+            prevRob = rob;
+        }
+        return Math.max(rob, notRob);
+    }
+
+    @Test
+    public void testHouseRobber() {
+        int[] exampleOne = {2,3,2};
+        int[] exampleTwo = {1,2,3,1};
+        assertEquals(3, houseRobber(exampleOne));
+        assertEquals(4, houseRobber(exampleTwo));
+    }
+}
+
+/*
+Explanation
+Bottom up DP Problem. It is just House Robber 1 and we call it twice, 1 going from 0 to n-2, and the other 1 to n-2
+
+Time: O(n)
+Space: O(n) or O(1)
+
+*/
