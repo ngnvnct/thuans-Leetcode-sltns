44f44b50c21b8cefca5d90f07ad7447e011c530f
Thuan Le <gile@ucsc.edu>
2022-02-11 14:55:27 -0800

    add jewels and stones, jump game 2 and remove nth in linked list

--- jewels_and_stones.java
+++ jewels_and_stones.java
@@ -1,3 +1,54 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
 public class jewels_and_stones {
-    
+    public int numJewelsInStonesSet(String jewels, String stones) {
+        int count = 0;
+        Set<Character> set = new HashSet<>();
+        for (Character j : jewels.toCharArray()) {
+            set.add(j);
+        }
+        for (Character s : stones.toCharArray()) {
+            if (set.contains(s)) {
+                count ++;
+            }
+        }
+        return count;
+    }
+
+    public int numJewelsInStones(String jewels, String stones) {
+        int count = 0;
+        for (char c : stones.toCharArray()) {
+            if (jewels.contains(c + "")) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Test
+    public void testJewels() {
+        String jewels1 = "aA";
+        String stones1 = "aAAbbbb";
+        String jewels2 = "z";
+        String stones2 = "ZZ";
+        assertEquals(3, numJewelsInStones(jewels1, stones1));
+        assertEquals(0, numJewelsInStones(jewels2, stones2));
+        assertEquals(3, numJewelsInStonesSet(jewels1, stones1));
+        assertEquals(0, numJewelsInStonesSet(jewels2, stones2));
+
+
+    }
 }
+
+/*
+Explanation
+A Set problem. Store the set and check if stones contain it.
+
+Time: O(m+n)
+Space: O(n) where N i the jewels
+*/
---
+++ jump_game_ii.java
@@ -0,0 +1,52 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class jump_game_ii {
+    public int jump(int[] nums) {
+        int res = 0;
+        int left = 0;
+        int right = 0;
+        while (right < nums.length-1) {
+            int farthest = 0;
+            for (int i = left; i < right+1; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+            left = right+1;
+            right = farthest;
+            res += 1;
+        }
+        return res;
+    }
+
+    public int jump2(int[] nums) {
+        int res = 0;
+        int currEnd = 0;
+        int currFarthest = 0;
+        for (int i = 0; i < nums.length-1; i++) {
+            currFarthest = Math.max(currFarthest, i + nums[i]);
+            if (i == currEnd) {
+                res++;
+                currEnd = currFarthest;
+            }
+        }
+        return res;
+    }
+
+    @Test
+    public void testJump() {
+        int[] exampleOne = {2,3,1,1,4};
+        assertEquals(2, jump(exampleOne));
+        assertEquals(2, jump2(exampleOne));
+    }
+}
+
+/*
+Explanation
+Jump1 is also a greedy BFS solution, the level tells how many step you need to reach each level. Left and right is a window. and the for loop will check for the farthest jump.
+
+Jump2 is a bfs solution, i == curEnd means I visited all the items on the current level, increment res++ is like incrementing the level you are on. currEnd = currFarthest is getting queue size
+for the next level I am traversing
+
+
+*/
