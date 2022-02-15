119885a6a83b94c30d71ef106a2dc90c6be8e872
Thuan Le <gile@ucsc.edu>
2022-02-14 23:20:34 -0800

    Add flood fill and number of islands and single number

---
+++ number_of_islands.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class number_of_islands {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int islands = 0;
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1') {
+                    islands += dfs(grid, i, j);
+                }
+            }
+        }
+        return islands;
+    }
+    public int dfs(char[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == '0') {
+            return 0;
+        }
+        grid[i][j] = '0';
+        dfs(grid, i+1,j);
+        dfs(grid, i-1, j);
+        dfs(grid, i, j+1);
+        dfs(grid, i,j -1);
+        return 1;
+    }
+
+    @Test public void testIslands() {
+        char[][] exampleOne = {
+            {'1','1','1','1','0'},
+            {'1','1','0','1','0'},
+            {'1','1','0','0','0'},
+            {'0','0','0','0','0'}
+        };
+        assertEquals(1, numIslands(exampleOne));
+
+        char[][] exampleTwo = {
+            {'1','1','0','0','0'},
+            {'1','1','0','0','0'},
+            {'0','0','1','0','0'},
+            {'0','0','0','1','1'}
+        };
+        assertEquals(3, numIslands(exampleTwo));
+    }
+
+}
+
+/*
+Explanation
+This is a Graph problem, which can be solve using Depth First Search (DFS) or BFS. DFS will be used here. First, we have a constraint if the length is 0 or it is null, return 0
+then we want to loop through the grid row and columns, and if we encounter a 1, we want to do something with it. In this case, we will call dfs to check for number of islands
+and add it to our final result.
+
+In our DFS call, we want to have a constraint check, if it is out of bound or we encounter a 0, we return 0
+else, we mark each visited "1" with a "0" so we don't revisit it again, then we call the dfs stack going down, up, to the left and to the right
+
+Time: O(n)
+Space: O(mn) because of recursion stack
+*/
--- single_number.java
+++ single_number.java
@@ -1,3 +1,36 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class single_number {
-    
+    public int singleNumber(int[] nums) {
+        int bitWise = 0;
+        for (int num : nums) {
+            bitWise ^= num;
+        }
+        return bitWise;
+    }
+
+    @Test
+    public void testSingleNumber() {
+        int[] expectedOne = {2,2,1};
+        int[] expectedTwo = {4,1,2,1,2};
+        int[] expectedThree = {1};
+        assertEquals(1, singleNumber(expectedOne));
+        assertEquals(4, singleNumber(expectedTwo));
+        assertEquals(1, singleNumber(expectedThree));
+    }
 }
+
+/*
+Explanation
+This is a bit manipulation problem. ^= is XOR, the integer becomes binary representation, and XOR will calculate as
+1 ^ 0 = 1
+0 ^ 1 = 1
+1 ^ 1 = 0
+0 ^ 0 = 0
+If you XOR the exact same number, they will become 0 in the output.
+
+Time: O(n)
+Space: O(1)
+*/
