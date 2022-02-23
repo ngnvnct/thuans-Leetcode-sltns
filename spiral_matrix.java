d31547cafd6b7dc3fe72c210aab5fac2ab8d72bf
Thuan Le <gile@ucsc.edu>
2022-02-22 21:50:21 -0800

    update spiral matrix

--- spiral_matrix.java
+++ spiral_matrix.java
@@ -2,6 +2,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class spiral_matrix {
+
+    public static void main (String[] args) {
+        spiral_matrix Solution = new spiral_matrix();
+        int[][] matrix = {{1,2,3}, {4,5,6}, {7,8,9}};
+        System.out.println(Solution.spiralOrder(matrix).toString());
+
+        int[][] matrixTwo = {{1,2,3,4}, {5,6,7,8}, {9,10,11,12}};
+        System.out.println(Solution.spiralOrder(matrixTwo).toString());
+
+    }
     List<Integer> spiralOrder(int[][] matrix) {
         List<Integer> res = new ArrayList<>();
 
@@ -39,3 +49,12 @@ public class spiral_matrix {
         return res;
     }
 }
+
+/*
+Explanation
+Think of it as the constraint shrinking
+First, we move from left to right, so it will be rowBegin = 0 matrix[rowBegin][i], and i is the column that we want to move. Then we increase the row begin
+Then, we want to go from rowBegin to rowEnd (down), so it will be matrix[i][colEnd] (because we want to do the last column). Then we decrease the col end
+When you traverse left or up, you need to check whether the row or col still exists to prevent duplicates. 
+
+*/
