ec9df1c03963ccacd18d6beb4c1d49a03a8bf445
Thuan Le <gile@ucsc.edu>
2022-03-22 22:26:27 -0700

    more problems, so hard

--- extract_string_to_integer.java
+++ extract_string_to_integer.java
@@ -24,7 +24,7 @@ public class extract_string_to_integer {
                 
                 if (Character.isDigit(c)) {
                     count = 10 * count + (c - '0');
-                    if (count == 0) {
+                    if (count == 0 && i == word.length()-1 || count == 0 && !Character.isDigit(word.charAt(i+1))) {
                         res.add(count);
                     }
                 } else {
@@ -50,6 +50,10 @@ public class extract_string_to_integer {
         String[] two = {"A2D", "1B", "3F5", "60FE2", "A0A"};
         List<Integer> expectedTwo = new ArrayList<>(Arrays.asList(2,1,3,5,60,2, 0));
         assertEquals(expectedTwo, extract(two));
+
+        String[] three = {"A2D", "1B", "3F5", "60FE022", "A0A0"};
+        List<Integer> expectedThree = new ArrayList<>(Arrays.asList(2,1,3,5,60,22,0,0));
+        assertEquals(expectedThree, extract(three));
     }
 }
 
@@ -61,6 +65,7 @@ if the last character is a number, have to do one last check outside to see if i
 
 One Edge case that i didn't catch, what if there is only 0 in the string, need the is count equals 0 after the calculation, if it is
 it only contains 0, so add it in the result
+Another edge case is leading 0, i don't want that
 
 One potential follow up is, what if i don't want to contain duplicate, use a set.
 
---
+++ wildcard_matching.java
@@ -0,0 +1,146 @@
+public class wildcard_matching {
+    public boolean isMatch(String query, String template) {
+        int queryLen = query.length(), pLen = template.length();
+        int queryIndex = 0, templateIndex = 0;
+        int starIndex = -1, queryTempIndex = -1;
+
+        while (queryIndex < queryLen) {
+            // If the pattern caracter = string character
+            // or pattern character = '?'
+            if (templateIndex < pLen && (template.charAt(templateIndex) == '?' || template.charAt(templateIndex) == query.charAt(queryIndex))) {
+                ++queryIndex;
+                ++templateIndex;
+    
+            // If pattern character = '*'
+            } else if (templateIndex < pLen && template.charAt(templateIndex) == '*') {
+                // Check the situation
+                // when '*' matches no characters
+                starIndex = templateIndex;
+                queryTempIndex = queryIndex;
+                ++templateIndex;
+                          
+            // If pattern character != string character
+            // or pattern is used up
+            // and there was no '*' character in pattern 
+            } else if (starIndex == -1) {
+                return false;
+                          
+            // If pattern character != string character
+            // or pattern is used up
+            // and there was '*' character in pattern before
+            } else {
+                // Backtrack: check the situation
+                // when '*' matches one more character
+                templateIndex = starIndex + 1;
+                queryIndex = queryTempIndex + 1;
+                queryTempIndex = queryIndex;
+            }
+        }
+
+        // The remaining characters in the pattern should all be '*' characters
+        for (int i = templateIndex; i < pLen; i++) {
+            if (template.charAt(i) != '*') {
+                return false;
+            }
+   
+        }
+        return true;
+    }
+
+    public boolean isMatchDP(String query, String template) {
+        if(query == null || template == null) return false;
+        int sLen = query.length();
+        int pLen = template.length();
+        boolean[][] dp = new boolean[sLen + 1][pLen + 1];
+        
+        // Base cases:
+        dp[0][0] = true;
+        // This for loop is unneeded because the boolean array is initalize to false
+        //first col: dp[i][0], can't match when template is empty, all false
+        //for(int i = 1; i <= sLen; i++){
+        //    dp[i][0] = false;
+        //}
+
+        //first row: dp[0][j] except for String template starts with *, otherwise all false
+        for(int j = 1; j <= pLen; j++){
+            if(template.charAt(j-1) == '*'){
+                dp[0][j] = dp[0][j-1];
+            }            
+        }
+        
+        // Recursion:
+        for(int i = 1; i <= sLen; i++){
+            for(int j = 1; j <= pLen; j++){
+                // the last && is a diagonal part
+                if((query.charAt(i-1) == template.charAt(j-1) || template.charAt(j-1) == '?') && dp[i-1][j-1]) {
+                    dp[i][j] = true;
+                }
+
+                // dp[i-1][j], means that "*" acts like an empty sequence (ab, ab*)
+                // for dp[i][j-1], means that * acts like any sequences (abcd, ab*)
+                else if (template.charAt(j-1) == '*' && (dp[i-1][j] || dp[i][j-1])) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[sLen][pLen];
+    }
+}
+
+
+/*abstract
+template: b*b*b*b*a
+query: bbbbbbbbbbbbbbbbba (essentially the idea here is that it has to at least be 'bbbba' but any number of b's will also be valid
+template: b*b*b*b*a
+output: True
+
+I have no idea how to explain the optimal solution
+       0  1 2 3 4 5
+       [] x ? y * z
+0 []   T  F F F F F
+1 x    F  T F F F F
+2 a    F  F T F F F
+3 y    F  F F T T F
+3 l    F  F F F T F
+4 m    F  F F F T F
+5 z    F  F F F T T
+
+       0  1 2 3 4 
+       [] * a * a
+0 []   T  T F F F
+1 b    F  T F F F
+2 b    F  T F F F
+3 a    F  T T T F
+4 a    F  T T T T
+
+
+T[0][0] empty string and empty pattern
+string "" and pattern "x", false
+first column if there is a string and no pattern, they will always be false
+
+T[i][j] = T[i-1][j-1] === T[i][j-1] T[i-1][j]
+
+x? == xa, ? can match with anything, 
+x?y == xay, look at diagonal value
+x?y* == xay, if * is a zero sequence, look at the value on the left
+
+DP Explanation
+dp[i][j]: true if the first i char in String query matches the first j chars in String template
+Base Case:
+   origin: dp[0][0] = true
+first row: dp[0][j] except for String template starts with *, otherwise all false
+first col: dp[i][0], can't match when template is empty, all false
+
+Recursion
+Iterate through every dp[i][j]
+dp[i][j] = true
+   if (query[ith] == template[jth] || template[jtth] == '?') && dp[i-1][j-1] = true (diagonal))
+   else if (template[jth] == '*' && dp[i-1][j] == true || dp[i][j-1] == true)
+        for dp[i][j], means that "*" acts like an empty sequence (ab, ab*)
+        for dp[i][j-1], means that * acts like any sequences (abcd, ab*)
+Start from 0 to len
+output should be dp[query.len][template.len]
+
+Time:  O(m x n)
+Space: O(m x n)
+*/
