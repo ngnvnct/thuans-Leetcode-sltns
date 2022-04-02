13e6dfec35bc4c06160b9c10a69acb2d561879e3
Thuan Le <gile@ucsc.edu>
2022-04-01 23:49:03 -0700

    find dupliactes and ransom note

---
+++ find_duplicates_array.java
@@ -0,0 +1,122 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class find_duplicates_array {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums) {
+            if (set.contains(num)) {
+                res.add(num);
+            }
+            set.add(num);
+        }
+        return res;
+    }
+
+    public List<Integer> findDuplicates2(int[] nums) {
+        Arrays.sort(nums);
+        List<Integer> res = new ArrayList<>();
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == nums[i-1]) {
+                res.add(nums[i]);
+                i++;
+            }
+        }
+        return res;
+    }
+
+    public List<Integer> findDuplicates3(int[] nums) {
+        int[] copy = nums.clone();
+        List<Integer> res = new ArrayList<>();
+
+        for (int num : copy) {
+            copy[Math.abs(num) - 1] *= -1;
+        }
+
+        for (int num : copy) {
+            if (copy[Math.abs(num) - 1] > 0) {
+                res.add(Math.abs(num));
+                copy[Math.abs(num) - 1] *= -1;
+            }
+        }
+        return res;
+    }
+
+    public List<Integer> findDuplicates4(int[] nums) {
+        int[] copy = nums.clone();
+        List<Integer> res = new ArrayList<>();
+
+        for (int num : copy) {
+            if (copy[Math.abs(num) -1] < 0) {
+                res.add(Math.abs(num));
+            }
+            copy[Math.abs(num) - 1] *= -1;
+        }
+        return res;
+    }
+
+    @Test
+    public void testDuplicate() {
+        int[] one = {4,3,2,7,8,2,3,1};
+        List<Integer> expectedOne = new ArrayList<>(Arrays.asList(2,3));
+        int[] two = {1,1,2};
+        List<Integer> expectedTwo = new ArrayList<>(Arrays.asList(1));
+        int[] three = {1};
+        List<Integer> expectedThree = new ArrayList<>();
+
+        assertEquals(expectedOne, findDuplicates(one));
+        assertEquals(expectedOne, findDuplicates2(one));
+        assertEquals(expectedOne, findDuplicates3(one));
+        assertEquals(expectedOne, findDuplicates4(one));
+        assertEquals(expectedTwo, findDuplicates(two));
+        assertEquals(expectedTwo, findDuplicates2(two));
+        assertEquals(expectedTwo, findDuplicates3(two));
+        assertEquals(expectedTwo, findDuplicates4(two));
+        assertEquals(expectedThree, findDuplicates(three));
+        assertEquals(expectedThree, findDuplicates2(three));
+        assertEquals(expectedThree, findDuplicates3(three));
+        assertEquals(expectedThree, findDuplicates4(three));
+    }
+}
+
+
+
+/*abstract
+Explanation
+
+Constraints: all num is positive
+Each element appears once or twice
+
+
+Sort Solution
+Time: O(n log n)
+Space: O(n) if count result, O(1)
+Works for both negative and positive
+
+Set, List Solution
+Time: O(n), it can be O(n^2), look up is constant, but it can degrade to linear
+Space: O(n)
+Set list works for both negative and positive value
+
+
+Mark Visited Elements as Negative
+We are using abs is because checking for nums[-1] doesn't make any sense
+When we see an element for the first time, we negate it
+If it is negative the next time we see it, it is a duplicate so we add it to the result
+
+Time: O(n)
+Space: O(n) if count output, else O(1), we re-use the input array
+
+
+JUnit Test: Using a copy so i don't modify the array
+*/
---
+++ ransom_note.java
@@ -0,0 +1,85 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+public class ransom_note {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        int[] alphabet = new int[26];
+        for (int i = 0; i < magazine.length(); i++) {
+            alphabet[magazine.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            int wordConstruct = alphabet[ransomNote.charAt(i) - 'a'];
+
+            if (wordConstruct <= 0) {
+                return false;
+            }
+            alphabet[ransomNote.charAt(i) - 'a']--;
+        }
+        return true;
+    }
+
+    public boolean canConstructMap(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length()) {
+            return false;
+        }
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < magazine.length(); i++) {
+            char c = magazine.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char c = ransomNote.charAt(i);
+            int wordConstruct = map.getOrDefault(c, 0);
+
+            if (wordConstruct == 0) {
+                return false;
+            }
+
+            map.put(c, wordConstruct - 1);
+        }
+        return true;
+    }
+    
+    @Test
+    public void testRansomNote() {
+        assertEquals(false, canConstruct("a", "b"));
+        assertEquals(false, canConstructMap("a", "b"));
+        assertEquals(false, canConstruct("aa", "ab"));
+        assertEquals(false, canConstructMap("aa", "ab"));
+        assertEquals(true, canConstruct("aa", "aab"));
+        assertEquals(true, canConstructMap("aa", "aab"));
+    }
+}
+
+/*
+ * Explanation
+ * 
+ * Using the bucket to count the alphabet, assuming all lower case
+ * then loop over to ransom and get the value, if it is <0, return false, we
+ * can't construct, else subtract the character by one. When the loop ends
+ * return true
+ * 
+ * Time: O(n)
+ * Space: O(26)
+ * 
+ * Two Hash Map ways: one for ransom and one for magazine
+ * Loop over each cahracter of the ransomnote, and check how many exists in the magazine
+ * if it isn't in the magazine, return false, if the count of magazine char is smaller than count of ransom note char, return false, else return true after loop
+ * 
+ * 
+ * One Hash Map (Optimal)
+ * Build it with magazine frequency, then subtract character from the ransom note.If the character isn't in the hashmap, assign it the value of 0
+ * if it is 0, return false
+ */
