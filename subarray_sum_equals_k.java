14925598e490e555f0eab9d06c2f57c51480768c
Thuan Le <gile@ucsc.edu>
2022-04-20 19:45:19 -0700

    upload

--- subarray_sum_equals_k.java
+++ subarray_sum_equals_k.java
@@ -37,14 +37,14 @@ public class subarray_sum_equals_k {
     public int subArrayEasy(int[] nums, int k) {
         int res = 0;
         int sum = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1);
+        Map<Integer, Integer> sumOccurrencesMap = new HashMap<>();
+        sumOccurrencesMap.put(0, 1);
         for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-            if (map.containsKey(sum-k)) {
-                res += map.get(sum-k);
+            if (sumOccurrencesMap.containsKey(sum-k)) {
+                res += sumOccurrencesMap.get(sum-k);
             }
-            map.put(sum, map.getOrDefault(sum, 0)+1);
+            sumOccurrencesMap.put(sum, sumOccurrencesMap.getOrDefault(sum, 0)+1);
         }
         return res;
     }
@@ -87,6 +87,22 @@ Store the current Sum in a hash map with a counter, then add to the result if th
 7
 Res = 4
 Sub Array [3,4], [7], [7,2,-3,1] and [1,4,2] are subarrays add up to k
+
+Better Explanation
+The HashMap will store with the key being any particular sum, and the value being the number of times it has happened till the current iteration of the loop
+
+k = 26
+sub-array sums up to k, then the sum at the end of this sub-array will be sumEnd = sumStart + k
+sumStart = sumEnd -k
+Suppose at index 10, sum = 50, next 6 numbers are 8, -5, -3, 10, 15,1
+At index 13, sum = 50 again, (8 + (-5) + (-3) = 0)
+At index 16, sum = 76
+sum -k = 76-26 = 50. So if this is the end index of a sub-array(s) which sums up to k, then before this, just before the start of the sub array, the sum should be 50
+We found sum =50 at two places before reaching index 16, we have two sub-arrays which sum up to k(26) (index 14 to 16) and indexes 11 to 16
+
+At each sum, if there is a presum exists, then there is a subarray between this sum and presum. 
+
+We put (0,1) for the a possible answer exist right at the start of the array
 
 Time: O(n)
 Space: O(n)
