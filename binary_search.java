46a71b1280a0b742931bf4b0ca5ab82e848ff5b3
Thuan Le <gile@ucsc.edu>
2022-04-22 01:50:57 -0700

    add exaplnation and test case for binary search

--- binary_search.java
+++ binary_search.java
@@ -4,15 +4,15 @@ import static org.junit.Assert.assertEquals;
 public class binary_search {
     public int binarySearch(int[] nums, int target) {
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             } else if (nums[mid] < target) {
-                left = mid+1;
+                left = mid + 1;
             } else {
-                right = mid-1;
+                right = mid - 1;
             }
         }
         return -1;
@@ -20,12 +20,23 @@ public class binary_search {
 
     @Test
     public void testBinarySearch() {
-        int[] exampleOne = new int[] {-1,0,3,5,9,12};
-        int target = 9;
-        assertEquals(4, binarySearch(exampleOne, target));
-
-        int[] exampleTwo = new int[] {-1,0,3,5,9,12};
-        target = 2;
-        assertEquals(-1, binarySearch(exampleTwo, target));
+        assertEquals(4, binarySearch(new int[] { -1, 0, 3, 5, 9, 12 }, 9));
+        assertEquals(-1, binarySearch(new int[] { -1, 0, 3, 5, 9, 12 }, 2));
     }
 }
+
+/*
+ * https://leetcode.com/problems/binary-search/
+ * Explanation
+ * 
+ * Textbook algorithm based on the idea to compare the target value ot the middle elemnt of the array.
+ * If the target value is equal to the middle element - we're done
+ * if the target value is smaller - search on the left
+ * if the target value is larger - search on the right
+ * 
+ * Works best when the array is SORTED.
+ * Use mid = left + (right - left) / 2 to avoid integer overflow vs left + right / 2
+ * 
+ * Time: O(log n)
+ * Space: O(1)
+ */
