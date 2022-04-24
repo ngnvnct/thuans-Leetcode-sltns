91289a6ca2225a6933dbcb89120a5a34e96cb298
Thuan Le <gile@ucsc.edu>
2022-04-24 01:00:14 -0700

    modify

--- max_nesting_depth_parentheses.java
+++ max_nesting_depth_parentheses.java
@@ -8,6 +8,11 @@ public class max_nesting_depth_parentheses {
         String one = "(1+(2*3)+((8)/4))+1";
         String two = "(1)+((2))+(((3)))";
         String three = "8*((1*(5+6))*(8/6))";
+
+        max_nesting_depth_parentheses sol = new max_nesting_depth_parentheses();
+        System.out.println(sol.maxDepth(one));
+        System.out.println(sol.maxDepth(two));
+        System.out.println(sol.maxDepth(three));
     }
 
     public int maxDepth(String s) {
@@ -49,7 +54,15 @@ public class max_nesting_depth_parentheses {
 
 /*
 Explanation
+Ask by Facebook Phone
+A naive solution is to use a stack, don't do that.
+Just ignore digits and signs and only count the ( or )
+Keep a variable currMax that is the Math.max of current max and max depth, everytime you see an open, you increment currMax and update a new max
+If you see a close parentheses, decrement curr max
 
+Not on leetcode, a facebook constraint is if a parentheses is unbalance, return -1, we have that if condition in the loop to terminate early
+At the end of the loop, we have to check if currMax is 0, if it is not zero that means it is unbalance, return -1
 
-
+Time: O(n)
+Space: O(1)
 */
