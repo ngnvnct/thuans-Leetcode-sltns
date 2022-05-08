3fd9199f95ba4b79531ca6ab3a5ccfdfe5a1cf51
Thuan Le <gile@ucsc.edu>
2022-05-07 22:08:46 -0700

    fix a parsum edge case and min add to make parenthesis valid

--- min_add_to_make_parenthesis_valid.java
+++ min_add_to_make_parenthesis_valid.java
@@ -24,7 +24,6 @@ public class min_add_to_make_parenthesis_valid {
         return count + stack.size();
     }
 
-
     public int minAddToMakeValid(String s) {
         int needLeft = 0;
         int needRight = 0;
@@ -53,7 +52,7 @@ public class min_add_to_make_parenthesis_valid {
 /*abstract
 Explanation
 Keep 2 counts variable, if we encounter a "(", we increment needRight
-else if we encoutner a ")", we check if a "(" count is already present, we decrement needRight
+else if we encounter a ")", we check if a "(" count is already present, we decrement needRight
 else we only encounter ")" so far, we have to increment needLeft to match it
 return needLeft + needRight
 
--- pairs_sum.java
+++ pairs_sum.java
@@ -22,6 +22,8 @@ public class pairs_sum {
                 }
                 digits = (c-'0');
                 countQuestionMarks = 0;
+            } else {
+                countQuestionMarks = 0;
             }
         }
         return sum == 10;
@@ -32,9 +34,14 @@ public class pairs_sum {
         String one = "asfafsasf5???5sdgsdgsg4???6";
         String two = "9???1???9???1";
         String three = "adsasdg4????6";
+        String four = "4?b??6";
+        String five = "4???6b";
         assertEquals(true, pairSum(one));
         assertEquals(true, pairSum(two));
         assertEquals(false, pairSum(three));
+        assertEquals(false, pairSum(four));
+        assertEquals(false, pairSum(""));
+        assertEquals(true, pairSum(five));
     }
 }
 
@@ -43,7 +50,9 @@ Explanation
 The brute force way would be storing the sum and then do another pass to check if that pairs of sum add up to 10
 
 But you can just have a condition to check if the current sum adds up to 10, return false if they are not
-at the end return sum == 10 instead of return true, because to get pass the edge case of 
+at the end return sum == 10 instead of return true, because to get pass the edge case of empty string
+
+We have to reset countQuestionMarks if we don't encounter 3 in a row (example four)
 
 Time: O(n)
 Space: O(1)
