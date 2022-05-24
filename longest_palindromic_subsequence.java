5fe75686b77b28e30f7a16c79c602a770f024c0f
Thuan Le <gile@ucsc.edu>
2022-05-24 01:51:43 -0700

    longest palindrome subsequence

--- longest_palindromic_subsequence.java
+++ longest_palindromic_subsequence.java
@@ -3,22 +3,22 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class longest_palindromic_subsequence {
-    
+
     public int longestPalindromeSubseq(String s) {
         if (s.isEmpty()) {
             return 0;
         }
 
-        int[][] dp = new int[s.length()+1][s.length()+1];
+        int[][] dp = new int[s.length() + 1][s.length() + 1];
         StringBuilder sbr = new StringBuilder(s);
         String rs = sbr.reverse().toString();
 
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 1; j <= rs.length(); j++) {
-                if (s.charAt(i-1) == rs.charAt(j-1)) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                if (s.charAt(i - 1) == rs.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
@@ -31,15 +31,15 @@ public class longest_palindromic_subsequence {
         }
         StringBuilder sbr = new StringBuilder(s);
         String rs = sbr.reverse().toString();
-        int[] previous = new int[s.length()+1];
-        int[] current = new int[s.length()+1];
+        int[] previous = new int[s.length() + 1];
+        int[] current = new int[s.length() + 1];
 
         for (int i = 1; i <= s.length(); i++) {
             for (int j = 1; j <= rs.length(); j++) {
-                if (s.charAt(i-1) == rs.charAt(j-1)) {
-                    current[j] = 1 + previous[j-1];
+                if (s.charAt(i - 1) == rs.charAt(j - 1)) {
+                    current[j] = 1 + previous[j - 1];
                 } else {
-                    current[j] = Math.max(previous[j], current[j-1]);
+                    current[j] = Math.max(previous[j], current[j - 1]);
                 }
             }
             int[] temp = previous;
@@ -61,13 +61,17 @@ public class longest_palindromic_subsequence {
 }
 
 /*
-https://leetcode.com/problems/longest-palindromic-subsequence/
-Explanation
-A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements
-
-Easy way to solve this problem is by turning it into a longest common subsequence problem.
-A palindrome reads the same forward as backward, so if we reversed the string, we can do LCS and find the longest LCS using Dynamic Programming
-
-Time: O(n^2)
-Space: O(n^2) or O(n) with Space optimized
-*/
+ * https://leetcode.com/problems/longest-palindromic-subsequence/
+ * Explanation
+ * A subsequence is a sequence that can be derived from another sequence by
+ * deleting some or no elements without changing the order of the remaining
+ * elements
+ * 
+ * Easy way to solve this problem is by turning it into a longest common
+ * subsequence problem.
+ * A palindrome reads the same forward as backward, so if we reversed the
+ * string, we can do LCS and find the longest LCS using Dynamic Programming
+ * 
+ * Time: O(n^2)
+ * Space: O(n^2) or O(n) with Space optimized
+ */
