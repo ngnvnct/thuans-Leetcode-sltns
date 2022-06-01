7c5841c74f717ba8213a4095deb66e9aee1bc245
Thuan Le <gile@ucsc.edu>
2022-05-31 20:29:09 -0700

    clean house

--- Climbing_Stairs_70.java
+++ Climbing_Stairs_70.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class climbing_stairs {
+public class Climbing_Stairs_70 {
     public int climbStairs(int n) {
         if (n <= 2) {
             return n;
@@ -25,18 +25,18 @@ public class climbing_stairs {
         if (n == 2) {
             return 2;
         }
-        return climbStairs(n-1) + climbStairs(n-2);
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 
     public int climbStairsDP(int n) {
         if (n == 1) {
             return 1;
         }
-        int[] dp = new int[n+1];
+        int[] dp = new int[n + 1];
         dp[1] = 1;
         dp[2] = 2;
         for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
         return dp[n];
     }
@@ -50,12 +50,14 @@ public class climbing_stairs {
 }
 
 /*
-Explantion:
-Use Dynamic Programming to store expensive calculation (memoization). Have a base case, then start with smaller values, and build a larger value using them.
-
-Time: O(n), n is input
-Space: O(1)
-
-Dynamic Programming: O(n) time, O(n) space
-Recursion: O(2^n) time, Space O(n)
-*/
+ * https://leetcode.com/problems/climbing-stairs/
+ * Explantion:
+ * Use Dynamic Programming to store expensive calculation (memoization). Have a
+ * base case, then start with smaller values, and build a larger value usingthem.
+ * 
+ * Time: O(n), n is input
+ * Space: O(1)
+ * 
+ * Dynamic Programming: O(n) time, O(n) space
+ * Recursion: O(2^n) time, Space O(n)
+ */
