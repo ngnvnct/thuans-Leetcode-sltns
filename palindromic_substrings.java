7a6af884cb74cb231bd2b88764120ad2db246606
Thuan Le <gile@ucsc.edu>
2022-06-18 16:57:08 -0700

    palindrome substring

--- palindromic_substrings.java
+++ palindromic_substrings.java
@@ -1,3 +1,7 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class palindromic_substrings {
     public int countSubstrings(String s) {
         if (s == null || s.length() < 1) return 0;
@@ -8,6 +12,17 @@ public class palindromic_substrings {
         }
         return ans;
     }
+
+    public int countSubstrings2(String s) {
+        if (s == null || s.length() < 1) return 0;
+        int ans = 0;
+        for (int i = 0; i < s.length(); i++) {
+            ans += expandAroundCenter2(s, i, i);
+            ans += expandAroundCenter2(s, i, i + 1);
+        }
+        return ans;
+    }
+
     private int expandAroundCenter(String s, int left, int right) {
         int ans = 0;
         while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
@@ -17,9 +32,30 @@ public class palindromic_substrings {
         }
         return ans;
     }
+    private int expandAroundCenter2(String s, int left, int right) {
+        int ans = 0;
+        while (left >= 0 && right < s.length()) {
+            if (s.charAt(left) != s.charAt(right)) {
+                break;
+            }
+            left--;
+            right++;
+            ans++;
+        }
+        return ans;
+    }
+
+    @Test
+    public void testPalindrome() {
+        assertEquals(3, countSubstrings("abc"));
+        assertEquals(6, countSubstrings("aaa"));
+        assertEquals(3, countSubstrings2("abc"));
+        assertEquals(6, countSubstrings2("aaa"));
+    }
 }
 
-/*abstract
+/*
+https://leetcode.com/problems/palindromic-substrings/
 Explanation
 
 Simliar to longest palindromic substring, expand outward from the center
