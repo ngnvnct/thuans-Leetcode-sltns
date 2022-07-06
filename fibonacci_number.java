4ae51ce59534f4039f3dabde45f012456832d37c
Thuan Le <gile@ucsc.edu>
2022-07-06 00:22:03 -0700

    add couple more solutions for fibonancci number

--- fibonacci_number.java
+++ fibonacci_number.java
@@ -7,7 +7,7 @@ public class fibonacci_number {
             return n;
         }
 
-        int sum  = 0;
+        int sum = 0;
         int[] memo = new int[2];
         memo[0] = 0;
         memo[1] = 1;
@@ -20,28 +20,65 @@ public class fibonacci_number {
         return sum;
     }
 
+    public int fib2(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int a = 0;
+        int b = 1;
+        int sum = 0;
+        while (n > 1) {
+            sum = a + b;
+            a = b;
+            b = sum;
+            n--;
+        }
+        return sum;
+    }
+
+    public int fibRecursion(int n) {
+        if (n < 2) {
+            return n;
+        }
+        return fib(n-1) + fib(n-2);
+    }
+
+    public int fibMath(int n) {
+        double goldenRatio = (1 + Math.sqrt(5)) / 2;
+        return (int) Math.round(Math.pow(goldenRatio, n) / Math.sqrt(5));
+    }
+
     @Test
     public void testFib() {
-        int expected = 1;
-        assertEquals(expected, fib(2));
-
-        expected = 2;
-        assertEquals(expected, fib(3));
-
-        expected = 3;
-        assertEquals(expected, fib(4));
-
-        expected = 832040;
+        assertEquals(1, fib(2));
+        assertEquals(2, fib(3));
+        assertEquals(3, fib(4));
         assertEquals(832040, fib(30));
+        assertEquals(1, fib2(2));
+        assertEquals(2, fib2(3));
+        assertEquals(3, fib2(4));
+        assertEquals(832040, fib2(30));
+        assertEquals(1, fibMath(2));
+        assertEquals(2, fibMath(3));
+        assertEquals(3, fibMath(4));
+        assertEquals(832040, fibMath(30));
+        assertEquals(1, fibRecursion(2));
+        assertEquals(2, fibRecursion(3));
+        assertEquals(3, fibRecursion(4));
+        assertEquals(832040, fibRecursion(30));
     }
 }
 
 /*
-Explantion:
-
-Use Dynamic Programming to store expensive calculation (memoization). Have a base case, then start with smaller values, and build a larger value using them.
-
-Time: O(n),
-Space: O(1),
-
-*/
+ * https://leetcode.com/problems/fibonacci-number/
+ * Explanation:
+ * 
+ * Use Dynamic Programming to store expensive calculation (memoization). Have a
+ * base case, then start with smaller values, and build a larger value using
+ * them.
+ * 
+ * The math solution is about some guy name Binet's and the golden ratio
+ * 
+ * Time: O(n)
+ * Space: O(1)
+ */
