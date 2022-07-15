be93aae5ec2a8d81b4bd5fa4b72172548a197719
Thuan Le <gile@ucsc.edu>
2022-07-14 20:35:55 -0700

    rename

--- Search_In_Rotated_Sorted_Array_33.java
+++ Search_In_Rotated_Sorted_Array_33.java
@@ -2,12 +2,12 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class search_rotated_sorted_array {
+public class Search_In_Rotated_Sorted_Array_33 {
     public int search(int[] nums, int target) {
         int left = 0;
-        int right = nums.length-1;
+        int right = nums.length - 1;
         while (left <= right) {
-            int mid = left + (right-left) / 2;
+            int mid = left + (right - left) / 2;
             if (target == nums[mid]) {
                 return mid;
             }
@@ -24,7 +24,7 @@ public class search_rotated_sorted_array {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
-                } 
+                }
             }
         }
         return -1;
@@ -32,14 +32,14 @@ public class search_rotated_sorted_array {
 
     public int searchMethod2(int[] nums, int target) {
         int left = 0;
-        int right = nums.length-1;
-        
+        int right = nums.length - 1;
+
         while (left <= right) {
-            int mid = left + (right-left) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            
+
             if (nums[left] <= nums[mid]) {
                 if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
@@ -59,14 +59,14 @@ public class search_rotated_sorted_array {
 
     public int searchMethod3(int[] nums, int target) {
         int left = 0;
-        int right = nums.length-1;
-        
+        int right = nums.length - 1;
+
         while (left < right) {
-            int mid = left + (right-left) / 2;
+            int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
                 return mid;
             }
-            
+
             if (nums[left] <= nums[mid]) {
                 if (target >= nums[left] && target < nums[mid]) {
                     right = mid - 1;
@@ -86,34 +86,44 @@ public class search_rotated_sorted_array {
 
     @Test
     public void testSearch() {
-        int[] exampleOne = {4,5,6,7,0,1,2};
-        int[] exampleTwo = {1};
-        assertEquals(4, search(exampleOne,0));
-        assertEquals(-1, search(exampleOne,3));
-        assertEquals(-1, search(exampleTwo,0));
-        assertEquals(4, searchMethod2(exampleOne,0));
-        assertEquals(-1, searchMethod2(exampleOne,3));
-        assertEquals(-1, searchMethod2(exampleTwo,0));
+        int[] exampleOne = { 4, 5, 6, 7, 0, 1, 2 };
+        int[] exampleTwo = { 1 };
+        
+        assertEquals(4, search(exampleOne, 0));
+        assertEquals(-1, search(exampleOne, 3));
+        assertEquals(-1, search(exampleTwo, 0));
+        assertEquals(4, searchMethod2(exampleOne, 0));
+        assertEquals(-1, searchMethod2(exampleOne, 3));
+        assertEquals(-1, searchMethod2(exampleTwo, 0));
     }
 }
 
 /*
-Explanation
-[4,5,6,7,0,1,2], we want to perform a binary search on it. Since it is rotated sorted array, we know that there will be an ascending left portion, and an ascending right portion.
-In this example, nums[mid] = 7, then we check if nums[left] <= nums[mid], if it is then we are searching the left portion
-if target is bigger than or equal to nums[left], and smaller than mid, meaning we don't care about the right portion, we shrink the window
-
-Search 2, If nums[mid] == target, the job is done, return mid
-Pivot element is larger than the first element in the array, i.e. the subarray from the first element to the pivot is non-rotated, as shown in the following graph.
-If the target is located in the non-rotated subarray:
-  go left: `right = mid - 1`.
-  else go right left = mid + 1
-Pivot element is smaller than the first element of the array, i.e. the rotation index is somewhere between 0 and mid. 
-It implies that the sub-array from the pivot element to the last one is non-rotated, as shown in the following graph.
-  - If the target is located in the non-rotated subarray:
-  go right: `start = mid + 1`.
-
-  - Otherwise: go left: `end = mid - 1`.
-Time: O(log n)
-Space: O(1)
-*/
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/
+ * Explanation
+ * [4,5,6,7,0,1,2], we want to perform a binary search on it. Since it is
+ * rotated sorted array, we know that there will be an ascending left portion,
+ * and an ascending right portion.
+ * In this example, nums[mid] = 7, then we check if nums[left] <= nums[mid], if
+ * it is then we are searching the left portion
+ * if target is bigger than or equal to nums[left], and smaller than mid,
+ * meaning we don't care about the right portion, we shrink the window
+ * 
+ * Search 2, If nums[mid] == target, the job is done, return mid
+ * Pivot element is larger than the first element in the array, i.e. the
+ * subarray from the first element to the pivot is non-rotated, as shown in the
+ * following graph.
+ * If the target is located in the non-rotated subarray:
+ * go left: `right = mid - 1`.
+ * else go right left = mid + 1
+ * Pivot element is smaller than the first element of the array, i.e. the
+ * rotation index is somewhere between 0 and mid.
+ * It implies that the sub-array from the pivot element to the last one is
+ * non-rotated, as shown in the following graph.
+ * - If the target is located in the non-rotated subarray:
+ * go right: `start = mid + 1`.
+ * 
+ * - Otherwise: go left: `end = mid - 1`.
+ * Time: O(log n)
+ * Space: O(1)
+ */
