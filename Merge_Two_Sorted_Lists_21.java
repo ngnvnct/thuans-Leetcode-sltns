fa380470453f1565c7911061c35904a72deb4ef5
Thuan Le <gile@ucsc.edu>
2022-07-24 13:31:10 -0700

    rename merge two sorted ll

--- Merge_Two_Sorted_Lists_21.java
+++ Merge_Two_Sorted_Lists_21.java
@@ -1,4 +1,4 @@
-public class merge_two_sorted_lists {    
+public class Merge_Two_Sorted_Lists_21 {
     public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode(-1);
         ListNode head = dummy;
@@ -23,16 +23,19 @@ public class merge_two_sorted_lists {
 }
 
 /*
-https://leetcode.com/problems/merge-two-sorted-lists/
-Explanation
-
-We use a dummy node, and which value is smaller, we take it and point it to the dummy node
-1->2->4
-1->3->4->5->6
-if we exhaust a list, another list can have still have some values after it, in that case, we just point our dummy.next to l2, and vice versa
-
-Time: O(n + m)
-Space: O(1)
-
-Write the test case in LinkedListTests.java
-*/
+ * Also appears on AlgoExpert
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ * Explanation
+ * 
+ * We use a dummy node, and which value is smaller, we take it and point it to
+ * the dummy node
+ * 1->2->4
+ * 1->3->4->5->6
+ * if we exhaust a list, another list can have still have some values after it,
+ * in that case, we just point our dummy.next to l2, and vice versa
+ * 
+ * Time: O(n + m)
+ * Space: O(1)
+ * 
+ * Write the test case in LinkedListTests.java
+ */
