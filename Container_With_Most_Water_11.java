7127351bd78070638e982f57c04fbc34021d8470
Thuan Le <gile@ucsc.edu>
2022-08-09 19:41:24 -0700

    Clean House

--- Container_With_Most_Water_11.java
+++ Container_With_Most_Water_11.java
@@ -2,15 +2,15 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-public class container_with_most_water {
+public class Container_With_Most_Water_11 {
     public int maxArea(int[] height) {
         int left = 0;
-        int right = height.length-1;
+        int right = height.length - 1;
         int res = 0;
         while (left < right) {
-            int containerLength = right-left;
+            int containerLength = right - left;
             int area = containerLength * Math.min(height[left], height[right]);
-            res = Math.max(res,area);
+            res = Math.max(res, area);
             if (height[left] < height[right]) {
                 left++;
             } else {
@@ -22,10 +22,10 @@ public class container_with_most_water {
 
     public int maxArea2(int[] height) {
         int left = 0;
-        int right = height.length-1;
+        int right = height.length - 1;
         int area = 0;
         while (left < right) {
-            int containerWidth = right-left;
+            int containerWidth = right - left;
             if (height[left] < height[right]) {
                 area = Math.max(area, height[left] * containerWidth);
                 left++;
@@ -40,8 +40,8 @@ public class container_with_most_water {
     public int maxAreaBF(int[] height) {
         int maxArea = 0;
         for (int left = 0; left < height.length; left++) {
-            for (int right = left+1; right < height.length; right++) {
-                maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right-left));
+            for (int right = left + 1; right < height.length; right++) {
+                maxArea = Math.max(maxArea, Math.min(height[left], height[right]) * (right - left));
             }
         }
         return maxArea;
@@ -49,8 +49,8 @@ public class container_with_most_water {
 
     @Test
     public void testMaxArea() {
-        int[] exampleOne = {1,8,6,2,5,4,8,3,7};
-        int[] exampleTwo = {1,1};
+        int[] exampleOne = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        int[] exampleTwo = { 1, 1 };
         assertEquals(49, maxArea(exampleOne));
         assertEquals(1, maxArea(exampleTwo));
         assertEquals(49, maxArea2(exampleOne));
@@ -60,15 +60,20 @@ public class container_with_most_water {
     }
 }
 
-/*abstract
-Explanation
-Using two pointers approach. We take the length of the right and left pointer, and multiply it by the minimum value of the height at left and the height at right, since the minimum value
-determines how much water we can hold. If the height at left is smaller than right, we move the left for a potential bigger height. If the left point and the right pointer have equal value,
-we can move either of them, it doesn't matter.
-
-Brute Force : Consider every single case of area
-Time: O(n^2), Space: O(1)
-
-Time: O(n)
-Space: O(1)
-*/
+/*
+ * https://leetcode.com/problems/container-with-most-water/
+ * Explanation
+ * Using two pointers approach. We take the length of the right and left
+ * pointer, and multiply it by the minimum value of the height at left and the
+ * height at right, since the minimum value determines how much water we can hold. 
+ * 
+ * If the height at left is smaller than
+ * right, we move the left for a potential bigger height. If the left point and
+ * the right pointer have equal value, we can move either of them, it doesn't matter.
+ * 
+ * Brute Force : Consider every single case of area
+ * Time: O(n^2), Space: O(1)
+ * 
+ * Time: O(n)
+ * Space: O(1)
+ */
