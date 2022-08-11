ca3a3e0951b7bee9a9f6619a71f163fe78e51281
Thuan Le <gile@ucsc.edu>
2022-08-10 19:41:38 -0700

    Clean House

--- Contains_Duplicate_217.java
+++ Contains_Duplicate_217.java
@@ -2,7 +2,7 @@ import java.util.*;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class contains_duplicate {
+public class Contains_Duplicate_217 {
     public static void main(String[] args) {
 
     }
@@ -21,7 +21,7 @@ public class contains_duplicate {
     public boolean containsDuplicateSort(int[] nums) {
         Arrays.sort(nums);
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] == nums[i-1]) {
+            if (nums[i] == nums[i - 1]) {
                 return true;
             }
         }
@@ -30,7 +30,7 @@ public class contains_duplicate {
 
     public boolean containsDuplicateBF(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
@@ -41,9 +41,9 @@ public class contains_duplicate {
 
     @Test
     public void testContainsDuplicate() {
-        int[] exampleOne = new int[]{1,2,3,1};
-        int[] exampleTwo = new int[]{1,2,3,4};
-        int[] exampleThree = new int[]{1,1,1,3,3,4,3,2,4,2};
+        int[] exampleOne = new int[] { 1, 2, 3, 1 };
+        int[] exampleTwo = new int[] { 1, 2, 3, 4 };
+        int[] exampleThree = new int[] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
         assertEquals(true, containsDuplicate(exampleOne));
         assertEquals(false, containsDuplicate(exampleTwo));
         assertEquals(true, containsDuplicate(exampleThree));
@@ -57,14 +57,19 @@ public class contains_duplicate {
 }
 
 /*
-Explanation
-Create a HashSet and add the element to it. As it implements the Set Interface, duplicate values are not allowed.
-If an element is a duplicate, it will return true.
-
-Brute Force: linear search, O(n^2) time and O(1) space
-
-Sorting: Sorting then linear search, O(n log n) time and O(1) space if heapsort is used. Althoug not a good idea to modify the input, make a copy isntead
-
-Time: O(n), lopping through the Array
-Space: O(n), storing the Array in a Set
-*/
+ * https://leetcode.com/problems/contains-duplicate/
+ * Explanation
+ * 
+ * Create a HashSet and add the element to it. As it implements the Set
+ * Interface, duplicate values are not allowed.
+ * If an element is a duplicate, it will return true.
+ * 
+ * Brute Force: linear search, O(n^2) time and O(1) space
+ * 
+ * Sorting: Sorting then linear search, O(n log n) time and O(1) space if
+ * heapsort is used. Althoug not a good idea to modify the input, make a copy
+ * isntead
+ * 
+ * Time: O(n), lopping through the Array
+ * Space: O(n), storing the Array in a Set
+ */
