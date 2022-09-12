2a00a9ffff8f3a3fd34107d9c1c7ac28a66114da
Thuan Le <gile@ucsc.edu>
2022-09-11 22:47:47 -0700

    clean up JUNITS

--- Can_Place_Flowers_605.java
+++ Can_Place_Flowers_605.java
@@ -3,9 +3,9 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 public class Can_Place_Flowers_605 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Can_Place_Flowers_605 Solution = new Can_Place_Flowers_605();
-        int[] exampleOne = {1,0,0,0,1};
+        int[] exampleOne = { 1, 0, 0, 0, 1 };
         System.out.println(Solution.canPlaceFlowersMethodTwo(exampleOne, 1));
     }
 
@@ -13,6 +13,7 @@ public class Can_Place_Flowers_605 {
         if (n == 0) {
             return true;
         }
+
         for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 0) {
                 if ((i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
@@ -24,16 +25,19 @@ public class Can_Place_Flowers_605 {
                 }
             }
         }
+
         return false;
     }
+
     public boolean canPlaceFlowersMethodTwo(int[] flowerbed, int n) {
         if (n == 0) {
             return true;
         }
-        for(int i = 0; i < flowerbed.length; i++){
+
+        for (int i = 0; i < flowerbed.length; i++) {
             if (flowerbed[i] == 0) {
-                int prev = (i == 0 || flowerbed[i-1] == 0) ? 0 : 1;
-                int next = (i == flowerbed.length-1 || flowerbed[i+1] == 0) ? 0 : 1;
+                int prev = (i == 0 || flowerbed[i - 1] == 0) ? 0 : 1;
+                int next = (i == flowerbed.length - 1 || flowerbed[i + 1] == 0) ? 0 : 1;
                 if (prev == 0 && next == 0) {
                     flowerbed[i] = 1;
                     n--;
@@ -43,17 +47,17 @@ public class Can_Place_Flowers_605 {
                 }
             }
         }
+
         return false;
     }
 
-
     @Test
     public void testPlaceFlower() {
-        int[] exampleOne = {1, 0, 1};
-        int[] exampleTwo = {1, 0, 0, 1};
-        int[] exampleThree = {1,0,0,0,1};
-        int[] exampleFour = {0, 0, 1};
-        int[] exampleFive = {1, 0, 0};
+        int[] exampleOne = { 1, 0, 1 };
+        int[] exampleTwo = { 1, 0, 0, 1 };
+        int[] exampleThree = { 1, 0, 0, 0, 1 };
+        int[] exampleFour = { 0, 0, 1 };
+        int[] exampleFive = { 1, 0, 0 };
 
         assertEquals(false, canPlaceFlowers(exampleOne, 1));
         assertEquals(false, canPlaceFlowers(exampleTwo, 1));
@@ -61,11 +65,11 @@ public class Can_Place_Flowers_605 {
         assertEquals(true, canPlaceFlowers(exampleFour, 1));
         assertEquals(true, canPlaceFlowers(exampleFive, 1));
 
-        exampleOne = new int[]{1, 0, 1};
-        exampleTwo = new int[]{1, 0, 0, 1};
-        exampleThree = new int[]{1,0,0,0,1};
-        exampleFour = new int[]{0, 0, 1};
-        exampleFive = new int[]{1, 0, 0};
+        exampleOne = new int[] { 1, 0, 1 };
+        exampleTwo = new int[] { 1, 0, 0, 1 };
+        exampleThree = new int[] { 1, 0, 0, 0, 1 };
+        exampleFour = new int[] { 0, 0, 1 };
+        exampleFive = new int[] { 1, 0, 0 };
 
         assertEquals(false, canPlaceFlowersMethodTwo(exampleOne, 1));
         assertEquals(false, canPlaceFlowersMethodTwo(exampleTwo, 1));
@@ -78,14 +82,16 @@ public class Can_Place_Flowers_605 {
 /*
  * https://leetcode.com/problems/can-place-flowers/
  * Explanation
- * Greedy algorithm. If you can place a flower there, do so, and check for edge case
+ * Greedy algorithm. If you can place a flower there, do so, and check for edge
+ * case
  * Edge Case
  * [1,0,1] no
  * [1,0,0,1] no
  * [1,0,0,0,1] yes
  * 
  * [0,0,1] and [1,0,0], two continous spot
- * JUnit Test, if you do two method, make sure to reinialize the test, since there is a chance the input array will be modify.
+ * JUnit Test, if you do two method, make sure to reinialize the test, since
+ * there is a chance the input array will be modify. You can prevent that by cloning an array to make JUnit tests more clean.
  * 
  * Time: O(n)
  * Space: O(1)
