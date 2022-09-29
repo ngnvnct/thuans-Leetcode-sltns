61eca3ff2d6011f4bf5cb07e78c416c13f4095bf
Thuan Le <gile@ucsc.edu>
2022-09-28 23:06:16 -0700

    rename

--- pairs_sum.java
+++ pairs_sum.java
@@ -1,8 +1,9 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
-public class pairs_sum {
+public class Pairs_Sum {
 
     public boolean pairSum(String input) {
         int digits = 0;
@@ -13,14 +14,14 @@ public class pairs_sum {
             if (c == '?') {
                 countQuestionMarks++;
             } else if (Character.isDigit(c) && countQuestionMarks != 3) {
-                digits = c -'0';
+                digits = c - '0';
                 countQuestionMarks = 0;
             } else if (Character.isDigit(c) && countQuestionMarks == 3) {
-                sum = digits + (c-'0');
+                sum = digits + (c - '0');
                 if (sum != 10) {
                     return false;
                 }
-                digits = (c-'0');
+                digits = (c - '0');
                 countQuestionMarks = 0;
             } else {
                 countQuestionMarks = 0;
@@ -31,29 +32,28 @@ public class pairs_sum {
 
     @Test
     public void testPairsSum() {
-        String one = "asfafsasf5???5sdgsdgsg4???6";
-        String two = "9???1???9???1";
-        String three = "adsasdg4????6";
-        String four = "4?b??6";
-        String five = "4???6b";
-        assertEquals(true, pairSum(one));
-        assertEquals(true, pairSum(two));
-        assertEquals(false, pairSum(three));
-        assertEquals(false, pairSum(four));
-        assertEquals(false, pairSum(""));
-        assertEquals(true, pairSum(five));
+        assertTrue(pairSum("asfafsasf5???5sdgsdgsg4???6"));
+        assertTrue(pairSum("9???1???9???1"));
+        assertFalse(pairSum("adsasdg4????6"));
+        assertFalse(pairSum("4?b??6"));
+        assertFalse(pairSum(""));
+        assertTrue(pairSum("4???6b"));
     }
 }
 
 /*
-Explanation
-The brute force way would be storing the sum and then do another pass to check if that pairs of sum add up to 10
-
-But you can just have a condition to check if the current sum adds up to 10, return false if they are not
-at the end return sum == 10 instead of return true, because to get pass the edge case of empty string
-
-We have to reset countQuestionMarks if we don't encounter 3 in a row (example four)
-
-Time: O(n)
-Space: O(1)
-*/
+ * Explanation
+ * The brute force way would be storing the sum and then do another pass to
+ * check if that pairs of sum add up to 10
+ * 
+ * But you can just have a condition to check if the current sum adds up to 10,
+ * return false if they are not
+ * at the end return sum == 10 instead of return true, because to get pass the
+ * edge case of empty string
+ * 
+ * We have to reset countQuestionMarks if we don't encounter 3 in a row (example
+ * four)
+ * 
+ * Time: O(n)
+ * Space: O(1)
+ */
