2961cac65eeace630f206640a4a8eb81ba86596b
Thuan Le <gile@ucsc.edu>
2022-10-22 11:36:46 -0700

    79

---
+++ Word_Search_79.java
@@ -0,0 +1,70 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Word_Search_79 {
+    public boolean wordSearch(char[][] board, String word) {
+        int row = board.length;
+        int col = board[0].length;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == word.charAt(0) && dfs(board, i, j, 0, word)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean dfs(char[][] board, int i, int j, int wordIndex, String word) {
+        if (wordIndex == word.length()) {
+            return true;
+        }
+
+        if (i < 0 || i >= board.length || j < 0 || j >= board[i].length || board[i][j] != word.charAt(wordIndex)) {
+            return false;
+        }
+        char temp = board[i][j];
+        board[i][j] = '#';
+        boolean found = dfs(board, i, j + 1, wordIndex + 1, word) ||
+                dfs(board, i - 1, j, wordIndex + 1, word) ||
+                dfs(board, i + 1, j, wordIndex + 1, word) ||
+                dfs(board, i, j - 1, wordIndex + 1, word);
+        board[i][j] = temp;
+        return found;
+    }
+
+    @Test
+    public void testWordSearch() {
+        char[][] boardOne = { { 'A', 'B', 'C', 'E' }, { 'S', 'F', 'C', 'S' }, { 'A', 'D', 'E', 'E' } };
+        String exampleOne = "ABCCED";
+        String exampleTwo = "SEE";
+        String exampleThree = "ABCB";
+        
+        assertTrue(wordSearch(boardOne, exampleOne));
+        assertTrue(wordSearch(boardOne, exampleTwo));
+        assertFalse(wordSearch(boardOne, exampleThree));
+    }
+}
+
+/*
+ * https://leetcode.com/problems/word-search/
+ * Explanation
+ * 
+ * This is a famous backtracking problem. If we see the first word in the word
+ * bank, we recursively call dfs on it.
+ * A B C E
+ * S F C S
+ * A D E E
+ * Word = ABCCED
+ * 
+ * It see A, it calls DFS, now A will go all 4 direction, and it found B, go all
+ * three directions, see C.....
+ * 
+ * Time: O(N * 3^L) where N is the number of cells and L is the number of length
+ * to be match, the 3 is the direction to explore, it is 4 direction but we
+ * don't need to revisit
+ * Space: O(L) where L is the number of length
+ */
