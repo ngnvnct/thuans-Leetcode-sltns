2ebccf856781329fa252fcec07af7899cea9ee6a
Thuan Le <gile@ucsc.edu>
2022-10-28 00:40:34 -0700

    236

--- Lowest_Common_Ancestor_Of_A_Binary_Tree_236.java
+++ Lowest_Common_Ancestor_Of_A_Binary_Tree_236.java
@@ -5,7 +5,7 @@ import java.util.Map;
 import java.util.Queue;
 import java.util.Set;
 
-public class lowest_common_ancestor_binary_tree_i {
+public class Lowest_Common_Ancestor_Of_A_Binary_Tree_236 {
     public TreeNode lca(TreeNode root, TreeNode p, TreeNode q) {
         if (root == null || root == p || root == q) {
             return root;
@@ -27,7 +27,7 @@ public class lowest_common_ancestor_binary_tree_i {
         }
         TreeNode left = lca2(root.left, p, q);
         TreeNode right = lca2(root.right, p, q);
-        
+
         if (left == null) {
             return right;
         } else if (right == null) {
@@ -41,6 +41,7 @@ public class lowest_common_ancestor_binary_tree_i {
         Queue<TreeNode> queue = new LinkedList<>();
         parent.put(root, null);
         queue.add(root);
+
         while (!parent.containsKey(p) || !parent.containsKey(q)) {
             TreeNode node = queue.poll();
             if (node != null) {
@@ -50,11 +51,14 @@ public class lowest_common_ancestor_binary_tree_i {
                 queue.add(node.right);
             }
         }
+
         Set<TreeNode> set = new HashSet<>();
+
         while (p != null) {
             set.add(p);
             p = parent.get(p);
         }
+
         while (!set.contains(q)) {
             q = parent.get(q);
         }
@@ -62,17 +66,19 @@ public class lowest_common_ancestor_binary_tree_i {
     }
 }
 
-/*abstract
-Explanation
-
-Traverse the tree in Depth first, the moment i encounter p or q, return some boolean flag. 
-Base case, we find p or q or touch the ground, return the root
-
-Then we search p and q from left and right
-From the root's left & right we found both p and q, so the root is the LCA
-Left is not null means from the left's left and right we found both p and q
-so the left is the lca, otherwise it is the right
-
-Time: O(n)
-Space: O(n)
-*/
+/*
+ * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+ * Explanation
+ * 
+ * Traverse the tree in Depth first, the moment i encounter p or q, return some
+ * boolean flag.
+ * Base case, we find p or q or touch the ground, return the root
+ * 
+ * Then we search p and q from left and right
+ * From the root's left & right we found both p and q, so the root is the LCA
+ * Left is not null means from the left's left and right we found both p and q
+ * so the left is the lca, otherwise it is the right
+ * 
+ * Time: O(n)
+ * Space: O(n)
+ */
