f4ec1588ec69530846dfda24e0de346eb35cbe1e
Spider-Man Noir <thuanl@compumailinc.com>
2023-01-09 12:48:25 -0800

    Create 1480. Running Sum of 1d Array

---
+++ csharp/solution_and_tests/1480-running-sum-of-1d-array.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Running_Sum_of_1d_Array
+{
+    class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            int[] runningSum = new int[nums.Length];
+            runningSum[0] = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                runningSum[i] = runningSum[i - 1] + nums[i];
+            }
+            return runningSum;
+        }
+
+        public int[] RunningSum2(int[] nums)
+        {
+            int[] runningSum = new int[nums.Length];
+            int totalSum = 0;
+            for (int i = 0; i < runningSum.Length; i++)
+            {
+                totalSum += nums[i];
+                runningSum[i] = totalSum;
+            }
+            return runningSum;
+        }
+    }
+
+    public class Tests
+    {
+        Solution sol;
+        [SetUp]
+        public void Setup()
+        {
+            sol = new Solution();
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4 }, ExpectedResult = new int[] { 1, 3, 6, 10 })]
+        [TestCase(new int[] { 1, 1, 1, 1, 1 }, ExpectedResult = new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 3, 1, 2, 10, 1 }, ExpectedResult = new int[] { 3, 4, 6, 16, 17 })]
+        public int[] TestRunningSum(int[] nums)
+        {
+            return sol.RunningSum(nums);
+        }
+
+        [Test]
+        [TestCase(new int[] { 1, 2, 3, 4 }, ExpectedResult = new int[] { 1, 3, 6, 10 })]
+        [TestCase(new int[] { 1, 1, 1, 1, 1 }, ExpectedResult = new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 3, 1, 2, 10, 1 }, ExpectedResult = new int[] { 3, 4, 6, 16, 17 })]
+        public int[] TestRunningSum2(int[] nums)
+        {
+            return sol.RunningSum2(nums);
+        }
+    }
+}
+
+/*
+ * https://leetcode.com/problems/running-sum-of-1d-array/
+ * Explanation
+ * 
+ * There is a solution which use the input array and modify it, not ideal. Can
+ * start the loop at 0 or 1 it doesn't matter
+ * For solution 2 there is one extra calculation, other than that pretty
+ * obvious.
+ * 
+ * Time: O(n)
+ * Space: O(1) if we don't care about output array
+ */
