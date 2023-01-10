a08ce084908ea573bc829467ab6c73e3f8fa7404
Spider-Man Noir <thuanl@compumailinc.com>
2023-01-09 16:39:35 -0800

    Create 804. Unique Morse Code Words

---
+++ csharp/solution_and_tests/0804-unique-morse-code-words.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Unique_Morse_Code_Words
+{
+    class Solution
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            string[] morseAlphabet = new string[] { ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..",
+                ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
+                "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.." };
+            HashSet<string> uniqueTransformation = new HashSet<string>();
+            foreach (string word in words)
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (char letter in word)
+                {
+                    sb.Append(morseAlphabet[letter - 'a']);
+                }
+                uniqueTransformation.Add(sb.ToString());
+            }
+            return uniqueTransformation.Count;
+        }
+    }
+
+    public class Tests
+    {
+        Solution sol;
+        [SetUp]
+        public void Setup()
+        {
+            sol = new Solution();
+        }
+
+        [Test]
+        [TestCase(new object[] { "gin", "zen", "gig", "msg" }, ExpectedResult = 2)]
+        [TestCase(new object[] { "a" }, ExpectedResult = 1)]
+        public int TestValidPalindrome(params string[] words)
+        {
+            return sol.UniqueMorseRepresentations(words);
+        }
+    }
+}
+
+/*
+ * https://leetcode.com/problems/unique-morse-code-words/
+ * 
+ * Put morse code out of the word in a string then add it to a hash set, return
+ * the size of the hash set is the unique morse code words
+ * 
+ * Time: O(n)
+ * Space: O(n)
+ */
