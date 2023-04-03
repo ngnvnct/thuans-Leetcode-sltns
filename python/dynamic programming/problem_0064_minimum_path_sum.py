6eeb47fcfab74577f3a97bd951f15919bc0acb4d
Vincent Nguyen <ngn.vincent@gmail.com>
2023-04-03 16:16:00 -0700

    minimum path sum

--- python/dynamic programming/problem_0064_minimum_path_sum.py
+++ python/dynamic programming/problem_0064_minimum_path_sum.py
@@ -11,18 +11,16 @@ class Solution:
         for row in range(1, rows):
             grid[row][0] += grid[row-1][0]
 
-        # nested for loop for filling in the inner cells. since there are only two directions to come from for each cell, we keep track of what the cell's value would be when coming from above or from the left, and then replace it with the minimum of the two values. 
+        # nested for loop for filling in the inner cells. since there are only 2 places we can arrive at this cell from (from above or from the left) then the minimum value between above and left will be the minimum path. the min value is added to the current cell 
         for row in range(1, rows):
             for col in range(1, cols):
-                from_above = grid[row][col] + grid[row-1][col]
-                from_left = grid[row][col] + grid[row][col-1]
-                grid[row][col] = min(from_above, from_left)
+                grid[row][col] += min(grid[row-1][col], grid[row][col-1])
         
         return grid[-1][-1]
     
 """
 We don't need to keep track of any old values in grid, so doing in place dp will be nice to make it space optimized. 
-Since you can only arrive to cells from either above or from the left, the cells in the very top row and leftmost column only have one possible place that a "pointer" can come from. Because of this, we can just sum up the values in those cells to start before working on the inner cells.
+Since you can only arrive to cells from either above or from the left, the cells in the very top row and leftmost column only have one possible place that a "pointer" can come from. Because of this, we can just sum up the values in those cells travelling in one direction to start before working on the inner cells.
 
 Time = O(mn)
 Space = O(1)
