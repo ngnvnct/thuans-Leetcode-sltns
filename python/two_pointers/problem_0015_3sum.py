4cd7e4a245ac87b4dd260fdc1c5038486aa7538b
Vincent Nguyen <ngn.vincent@gmail.com>
2023-04-09 00:21:08 -0700

    fixed 3sum

--- python/two_pointers/problem_0015_3sum.py
+++ python/two_pointers/problem_0015_3sum.py
@@ -3,21 +3,32 @@ import unittest
 class Solution:
     @staticmethod
     def threeSum(nums):
-        ans = set()
         nums.sort()
+        ans = []
         for left in range(len(nums)):
+            if left > 0 and nums[left] == nums[left-1]:
+                continue
             mid, right = left+1, len(nums)-1
             while mid < right:
                 sum = nums[left] + nums[mid] + nums[right]
+                # moves the mid and right pointers in to get closer to 0 based on certain conditions
                 if sum < 0:
                     mid += 1
+                    continue
                 elif sum > 0:
                     right -= 1
-                else:
-                    ans.add(tuple((nums[left], nums[mid], nums[right])))
+                    continue
+                # once the sum is 0, do this stuff
+                ans.append([nums[left], nums[mid], nums[right]])
+                # to avoid duplicates, move the pointers in to skip duplicate entries
+                while mid > right and nums[mid] == nums[mid+1]:
                     mid += 1
+                while left < right and nums[right] == nums[right-1]:
                     right -= 1
-        return ans
+                mid += 1
+                right -= 1
+
+        return list(ans)
     
 class TestCase(unittest.TestCase):
     def setUp(self):
