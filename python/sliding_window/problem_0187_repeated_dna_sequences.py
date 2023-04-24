1a0802bbd90de96bd6483281a8e528b6d06d6882
Vincent Nguyen <ngn.vincent@gmail.com>
2023-04-24 01:32:18 -0700

    problem_0187_repeated_dna_sequences.py python https://leetcode.com/problems/repeated-dna-sequences/submissions/888634868/

---
+++ python/sliding_window/problem_0187_repeated_dna_sequences.py
@@ -0,0 +1,33 @@
+import unittest
+
+class Solution:
+    @staticmethod
+    def findRepeatedDnaSequences(s):
+        seen, ans = set(), set()
+
+        for i in range(len(s)-9):   # s[n-9] is the highest index i can go to to ensure that the window of size 10 doesn't go off the string
+            subarray = s[i:i+10]
+            if subarray in seen:
+                ans.add(subarray)
+            seen.add(subarray)
+
+        return list(ans)        # leetcode lets you just return ans as a set and it accepts it
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+        self.testCase1 = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+        self.testCase1ExpectedResult = ["AAAAACCCCC","CCCCCAAAAA"]
+
+        self.testCase2 = "AAAAAAAAAAAAA"
+        self.testCase2ExpectedResult = ["AAAAAAAAAA"]
+    
+    def testFindRepeatedDnaSequences1(self):
+        self.assertEqual(self.solution.findRepeatedDnaSequences(self.testCase1), self.testCase1ExpectedResult)
+    
+    def testFindRepeatedDnaSequences2(self):
+        self.assertEqual(self.solution.findRepeatedDnaSequences(self.testCase2), self.testCase2ExpectedResult)
+
+if __name__ == "__main__":
+    unittest.main()
