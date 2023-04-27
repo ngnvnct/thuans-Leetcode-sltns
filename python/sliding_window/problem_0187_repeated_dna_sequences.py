05909ea123b867313f2428e72b51efbaed558272
Vincent Nguyen <ngn.vincent@gmail.com>
2023-04-27 00:10:49 -0700

    fixed

--- python/sliding_window/problem_0187_repeated_dna_sequences.py
+++ python/sliding_window/problem_0187_repeated_dna_sequences.py
@@ -3,12 +3,12 @@ import unittest
 class Solution:
     @staticmethod
     def findRepeatedDnaSequences(s):
-        seen, ans = set(), set()
+        seen, ans = set(), []
 
         for i in range(len(s)-9):   # s[n-9] is the highest index i can go to to ensure that the window of size 10 doesn't go off the string
             subarray = s[i:i+10]
-            if subarray in seen:
-                ans.add(subarray)
+            if subarray in seen and subarray not in ans:
+                ans.append(subarray)
             seen.add(subarray)
 
         return list(ans)        # leetcode lets you just return ans as a set and it accepts it
@@ -18,7 +18,7 @@ class TestCase(unittest.TestCase):
         self.solution = Solution()
 
         self.testCase1 = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
-        self.testCase1ExpectedResult = ["AAAAACCCCC","CCCCCAAAAA"] or ["CCCCCAAAAA","AAAAACCCCC"]
+        self.testCase1ExpectedResult = ["AAAAACCCCC","CCCCCAAAAA"]
 
         self.testCase2 = "AAAAAAAAAAAAA"
         self.testCase2ExpectedResult = ["AAAAAAAAAA"]
